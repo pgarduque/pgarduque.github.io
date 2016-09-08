930ebfd4c0ac43892f52485c9f1483c4ea6c7948
pgarduque <pgarduque@yahoo.com>
2016-09-07 20:22:14 -0700

    Delete String methods pt.2.rb

--- ruby_code/String methods pt.2.rb
+++
@@ -1,6 +0,0 @@
-puts "12345".length
-puts "desserts".reverse
-puts "one sentence \n two sentence \n".lines.to_a.inspect
-puts "a really fat dog". split(" ").inspect
-puts "spaces".strip
-puts "nom "*3
